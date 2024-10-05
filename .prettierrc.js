ffc991e70c1a22dfa6c8687e46453e8a5a4e2ea3
TuanNghia295 <tuannghiait2905@gmail.com>
2024-10-05 16:07:46 +0700

    Khởi tạo source dự án

---
+++ .prettierrc.js
@@ -0,0 +1,13 @@
+module.exports = {
+  arrowParens: 'avoid',
+  bracketSameLine: true,
+  bracketSpacing: true,
+  singleQuote: true,
+  trailingComma: 'es5',
+  printWidth: 80,
+  tabWidth: 2,
+  useTabs: false,
+  semi: true,
+  jsxBracketSameLine: false,
+  endOfLine: 'lf'
+};
