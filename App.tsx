7c4b5fbf92719473155410d1a98d0deaba781593
TuanNghia295 <tuannghiait2905@gmail.com>
2024-10-15 14:17:25 +0700

    Cấu hình FCm Token

--- App.tsx
+++ App.tsx
@@ -1,10 +1,32 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import {StatusBar} from 'react-native';
 import {NavigationContainer} from '@react-navigation/native';
 import AppRouter from './src/navigation/AppRouter';
 import {appColors} from './src/constants/colors';
+import messaging from '@react-native-firebase/messaging';
 
 const App = () => {
+  async function requestUserPermission() {
+    const authStatus = await messaging().requestPermission();
+    const enabled =
+      authStatus === messaging.AuthorizationStatus.AUTHORIZED ||
+      authStatus === messaging.AuthorizationStatus.PROVISIONAL;
+
+    if (enabled) {
+      console.log('Authorization status:', authStatus);
+    }
+  }
+
+  const getToken = async () => {
+    const token = await messaging().getToken();
+    console.log('token', token);
+  };
+
+  useEffect(() => {
+    requestUserPermission();
+    getToken();
+  }, []);
+
   return (
     <>
       <StatusBar barStyle={'dark-content'} backgroundColor={appColors.white} />
